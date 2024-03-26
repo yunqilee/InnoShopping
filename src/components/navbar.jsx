ab74bbfd002b553e54fc9c23509fc8ff6c92192e
yunqilee <yli380479@gmail.com>
2024-03-26 16:56:19 -0500

    set the navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import {ShoppingBag} from "phosphor-react";
+import {ShoppingCart} from "phosphor-react";
+import "./navbar.css"
 
 export const Navbar = () => {
-    return <div>Navbar</div>;
+    return <div className="navbar">
+        <h1 className="logo">InnoShopping</h1>
+        <div className="links">
+            <Link to={"/"}>
+                <ShoppingBag size={24} />
+            </Link>
+            <Link to={"/cart"}>
+                <ShoppingCart size={24} />
+            </Link>
+        </div>
+    </div>;
 }
