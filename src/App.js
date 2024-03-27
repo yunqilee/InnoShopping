a49ac113ee8e5c63fcebea8c1e49e860f9ac0bc0
yunqilee <yli380479@gmail.com>
2024-03-26 23:05:54 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import {Navbar} from "./components/navbar";
+import {Cart} from "./pages/cart/Cart";
+import {Product} from "./pages/shopping/Product";
 
 function App() {
   return (
@@ -8,8 +10,8 @@ function App() {
       <Router>
           <Navbar />
           <Routes>
-              <Route path={"/"}></Route>
-              <Route path={"/cart"}></Route>
+              <Route path={"/"} element={<Product />}></Route>
+              <Route path={"/cart"} element={<Cart />}></Route>
           </Routes>
       </Router>
     </div>
