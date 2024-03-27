36d2031b9f09c4bcfcfa552add26bc0bcc354d9b
yunqilee <yli380479@gmail.com>
2024-03-27 17:53:34 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import {Navbar} from "./components/navbar/navbar";
 import {Cart} from "./components/cart/Cart";
 import {Product} from "./components/shopping/Product";
+import {ShopContextProvider} from "./components/context/shop-context";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-          <Navbar />
-          <Routes>
-              <Route path={"/"} element={<Product />}></Route>
-              <Route path={"/cart"} element={<Cart />}></Route>
-          </Routes>
-      </Router>
+      <ShopContextProvider>
+          <Router>
+              <Navbar />
+              <Routes>
+                  <Route path={"/"} element={<Product />}></Route>
+                  <Route path={"/cart"} element={<Cart />}></Route>
+              </Routes>
+          </Router>
+      </ShopContextProvider>
     </div>
   );
 }
