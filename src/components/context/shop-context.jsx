6ff1181ee028ce8961b991c7d093f9a1334291a8
yunqilee <yli380479@gmail.com>
2024-03-27 17:48:26 -0500

    Create shop-context.jsx

---
+++ src/components/context/shop-context.jsx
@@ -0,0 +1,42 @@
+import {createContext, useState, useEffect} from "react";
+
+export const ShopContext = createContext(null);
+
+const getDefaultCart = async () => {
+    try {
+        const response = await fetch('https://dummyjson.com/products');
+        const data = await response.json();
+        const initialCart = {};
+        data.products.forEach(product => {
+            initialCart[product.id] = 0;
+        });
+        return initialCart;
+    } catch (error) {
+        console.error("Failed to fetch products:", error);
+        return {};
+    }
+};
+export const ShopContextProvider = (props) => {
+    const [cartItems, setCartItems] = useState({})
+
+    useEffect(() => {
+        const initializeCart = async () => {
+            const defaultCart = await getDefaultCart();
+            setCartItems(defaultCart)
+        }
+
+        initializeCart();
+    }, []);
+    const addToCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    };
+
+    const contextValue = {cartItems, addToCart}
+
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+
+}
