e2c37c59cda1175d64a47dc9b291eafd2cd70f02
yunqilee <yli380479@gmail.com>
2024-03-26 23:21:39 -0500

    Update Product.jsx

--- src/pages/shopping/Product.jsx
+++ src/pages/shopping/Product.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 export const Product = () => {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        fetch('https://dummyjson.com/products')
+            .then(response => response.json())
+            .then(data => setProducts(data.products))
+            .catch(error => console.error('Error fetching data: ', error));
+    }, []);
     return (
-        <h3>Product</h3>
+        <div className="products">
+            <div className="container grid3">
+                {products.map(product => (
+                    <div key={product.id}>
+                        <h3>{product.title}</h3>
+                        <img src={product.thumbnail} alt={product.title} style={{width: '100px', height: '100px'}}/>
+                        <p>{product.description}</p>
+                        <p>Price: ${product.price}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
