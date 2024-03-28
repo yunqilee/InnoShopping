819bff20a6503101ffbaddc23c003b292aa4b5d3
yunqilee <yli380479@gmail.com>
2024-03-28 16:37:47 -0500

    Update Product.jsx

--- src/components/shopping/Product.jsx
+++ src/components/shopping/Product.jsx
@@ -6,6 +6,7 @@ export const Product = () => {
     const [products, setProducts] = useState([])
     const {cartItems, addToCart} = useContext(ShopContext)
 
+
     useEffect(() => {
         fetch('https://dummyjson.com/products')
             .then(response => response.json())
@@ -16,17 +17,22 @@ export const Product = () => {
     return (
         <div className="product">
             <div className="container grid3">
-                {products.map(product => (
-                    <div key={product.id}>
-                        <h3>{product.title}</h3>
-                        <img src={product.thumbnail} alt={product.title} style={{width: '100px', height: '100px'}}/>
-                        <p>{product.description}</p>
-                        <p className="product-price">Price: ${product.price}</p>
-                        <button className="addToCartBtn" onClick={() => addToCart(product.id)}>
-                            Add to Cart
-                        </button>
-                    </div>
-                ))}
+                {products.map(product => {
+
+                    const cartItemsCount = cartItems[product.id] || 0;
+
+                    return (
+                        <div key={product.id}>
+                            <h3>{product.title}</h3>
+                            <img src={product.thumbnail} alt={product.title} style={{width: '100px', height: '100px'}}/>
+                            <p>{product.description}</p>
+                            <p className="product-price">Price: ${product.price}</p>
+                            <button className="addToCartBtn" onClick={() => addToCart(product.id)}>
+                                Add to Cart {cartItemsCount > 0 && <> ({cartItemsCount})</>}
+                            </button>
+                        </div>
+                    );
+                })}
             </div>
         </div>
     )
