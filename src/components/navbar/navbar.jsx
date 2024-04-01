e244dc0337cb1aa8560af20a8186012a1cb912ab
yunqilee <yli380479@gmail.com>
2024-03-31 21:26:12 -0500

    set the Navbar component

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
-import {ShoppingBag} from "phosphor-react";
-import {ShoppingCart} from "phosphor-react";
+import {ShoppingBag, ShoppingCart, MagnifyingGlass} from "phosphor-react";
 import "./navbar.css"
 
 export const Navbar = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearch = (event) => {
+        event.preventDefault();
+        console.log('Search Term:', searchTerm)
+    }
+
     return <div className="navbar">
         <Link to={"/"} className="logo-link">
             <h1 className="logo">InnoShopping</h1>
         </Link>
         <div className="links">
+            <form onSubmit={handleSearch} className="search-form">
+                <input
+                    className="search-input"
+                    type="text"
+                    placeholder="Search products..."
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <button type="submit">
+                    <MagnifyingGlass size={24}/>
+                </button>
+            </form>
             <Link to={"/"}>
-                <ShoppingBag size={24} />
+                <ShoppingBag size={24}/>
             </Link>
             <Link to={"/cart"}>
-                <ShoppingCart size={24} />
+                <ShoppingCart size={24}/>
             </Link>
         </div>
     </div>;
