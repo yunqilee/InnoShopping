f25265ad15c95b2d17fd314d38c5a032a38790bb
yunqilee <yli380479@gmail.com>
2024-05-06 18:04:00 -0500

    feat: Connect to MongoDB database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,19 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config({ path: ".env" });
 const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect(process.env.MONGODB_URI, {
+    authSource: "admin"
+})
+    .then(() => console.log('MongoDB connection successful'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
 
 app.listen(3001, () => console.log("SERVER STARTED"));
