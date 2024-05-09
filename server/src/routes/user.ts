c0a278357040f8cd513bf9ab208c3bacdc2c4816
yunqilee <yli380479@gmail.com>
2024-05-09 15:02:53 -0500

    feat: Add user login

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,6 +1,7 @@
 import {Router, Request, Response} from "express";
 import bcrypt from "bcrypt";
-import {UserModel} from "../models/user";
+import jwt from "jsonwebtoken";
+import {IUser, UserModel} from "../models/user";
 import {UserErrors} from "../common/errors";
 
 const router = Router();
@@ -20,6 +21,28 @@ router.post("/register", async (req: Request, res: Response) => {
         await newUser.save();
 
         res.json({message: "User registered successfully"})
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+})
+
+router.post("/login", async (req: Request, res: Response) => {
+    const {username, password} = req.body;
+    try {
+        const user: IUser = await UserModel.findOne({username});
+
+        if (!user) {
+            return res.status(400).json({type: UserErrors.NO_USER_FOUND})
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            return res.status(400).json({type: UserErrors.WRONG_CREDENTIALS})
+        }
+
+        const token = jwt.sign({id: user._id}, "secret")
+        res.json({token, userId: user._id})
+
     } catch (error) {
         res.status(500).json({error: error.message})
     }
