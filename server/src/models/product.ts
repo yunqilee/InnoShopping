e7d9ee0efab6b8c6135a34074eefb78e5f24af43
yunqilee <yli380479@gmail.com>
2024-05-18 13:49:06 -0500

    feat: Add product model

---
+++ server/src/models/product.ts
@@ -0,0 +1,19 @@
+import {Schema, model} from 'mongoose'
+
+export interface Iproduct {
+    title: string;
+    price: number;
+    thumbnail: string;
+    description: string;
+    stockQuantity: number;
+}
+
+const ProductSchema = new Schema<Iproduct>({
+    title: { type: String, required: true },
+    price: { type: Number, required: true, min: [0.99, "Price should be above 0.99"] },
+    thumbnail: { type: String, required: true },
+    description: { type: String, required: true },
+    stockQuantity: { type: Number, required: true, min: [0, "Stock can't be lower than 0"] },
+})
+
+export const ProductModel = model<Iproduct>('product', ProductSchema)
