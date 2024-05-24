77d550f1a465b89ec043e10e6ff67c46695192dc
yunqilee <yli380479@gmail.com>
2024-05-24 17:10:05 -0500

    feat: Add out of stock for product display page

--- client/src/components/shopping/Product.jsx
+++ client/src/components/shopping/Product.jsx
@@ -5,9 +5,15 @@ export const Product = ({product, addToCart, cartItemsCount}) => {
             <img src={product.thumbnail} alt={product.title} style={{width: '100px', height: '100px'}}/>
             <p>{product.description}</p>
             <p className="product-price">Price: ${product.price}</p>
-            <button className="addToCartBtn" onClick={() => addToCart(product._id)}>
+            <button className="addToCartBtn"
+                    onClick={() => addToCart(product._id)}
+                    disabled={product.stock === 0}>
                 Add to Cart {cartItemsCount > 0 && <> ({cartItemsCount})</>}
             </button>
+
+            <div className="stockQuantity">
+                {product.stock === 0 && <h3>out of stock</h3>}
+            </div>
         </div>
     )
 }
